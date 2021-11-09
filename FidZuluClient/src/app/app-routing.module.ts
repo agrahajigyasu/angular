f7ea48906f41376902eff74e1f05883229f37839
a666161 <sdmuhsin@gmail.com>
2021-11-09 08:37:17 -0500

    Initial structure

---
+++ FidZuluClient/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CategoryGalleryComponent } from './components/category-gallery/category-gallery.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+const routes: Routes = [
+  {path:'', component : CategoryGalleryComponent},
+  {path:'products/:category',component:ProductListComponent},
+  {path:'product/:id',component:ProductDetailsComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FidZuluClient/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CategoryGalleryComponent } from './components/category-gallery/category-gallery.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoryGalleryComponent,
+    ProductListComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
