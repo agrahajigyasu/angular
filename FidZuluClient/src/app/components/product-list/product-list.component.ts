e67de9bd398c4812050670fbb09ed37f5be60a50
a662322 <yogitasangwannit21@gmail.com>
2021-11-09 23:22:42 -0500

    updating product list

--- FidZuluClient/src/app/components/product-list/product-list.component.ts
+++ FidZuluClient/src/app/components/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Book } from 'src/app/book';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-product-list',
