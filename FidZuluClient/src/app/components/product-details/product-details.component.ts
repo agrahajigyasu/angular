7f0569c0af7317775612684e7176cbcca2f0ea32
vishalgb0403 <vishalgb0403@gmail.com>
2021-11-10 03:09:49 -0500

    t

--- FidZuluClient/src/app/components/product-details/product-details.component.ts
+++ FidZuluClient/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { KeyValue } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Book } from 'src/app/models/book';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,40 +11,46 @@ import { Book } from 'src/app/models/book';
 })
 export class ProductDetailsComponent implements OnInit {
   productId:any = -1;
-  category: String = "book";
+  category: string = "";
+  classType: string = "";
   books: Book[] = []
 
-  objects: any[] = [];
+  objects: any = [];
   object: any = null;
 
   
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private dataService : DataService) { }
 
   ngOnInit(): void {
     this.productId = this.route.snapshot.params['id'];
+    this.category = this.route.snapshot.params['category'];
+    this.classType =  this.route.snapshot.params['class'];
 
 
-    if(this.category == "book")
-      this.objects =  [
-        {
-          "Title": "Lord of the Rings",
-          "Author": "J.R.R Tolkien",
-          "price": 25.99,
-          "ISBN": "9780261102385",
-          "publisher": "HarperCollins"
-        },
-        {
-          "Title": "The Hobbit",
-          "Author": "J.R.R Tolkien",
-          "price": 9.88,
-          "ISBN": "0261102214",
-          "publisher": "HarperCollins"
-        }
-      ];
-
-      this.object = this.objects[this.productId];
-
+      // this.objects =  [
+      //   {
+      //     "Title": "Lord of the Rings",
+      //     "Author": "J.R.R Tolkien",
+      //     "price": 25.99,
+      //     "ISBN": "9780261102385",
+      //     "publisher": "HarperCollins"
+      //   },
+      //   {
+      //     "Title": "The Hobbit",
+      //     "Author": "J.R.R Tolkien",
+      //     "price": 9.88,
+      //     "ISBN": "0261102214",
+      //     "publisher": "HarperCollins"
+      //   }
+      // ];
+      this.dataService.getProducts(this.classType,this.category).subscribe(data =>{
+        this.objects = data;
+        this.object = this.objects[this.productId];
+        console.log(this.objects);
+        });
+      
+    
       
   }
 
